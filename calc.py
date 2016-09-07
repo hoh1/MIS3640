5ef66cc5fefff9924ebac93a0f64d35bf357e906
hoh1 <hoh1@babson.edu>
2016-09-06 20:27:40 -0400

    Exercise 2
    In_Class Activity_Exercise 2

---
+++ calc.py
@@ -0,0 +1,13 @@
+#Exercise 2_1
+print((60*42)+42)
+
+#Exercise 2_2
+print(10/1.61)
+
+#Exercise 2_3_a
+print(((60*42)+42)/(10/1.61))
+
+#Exercise 2_3_b
+print((10/1.61)/(((60*42)+42)/3600))
+
+input()
