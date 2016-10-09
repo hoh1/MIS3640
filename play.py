c9bf796b6549a1bda3b45fe1c690915fc944217e
hoh1 <hoh1@babson.edu>
2016-10-08 23:39:54 -0400

    practice

--- play.py
+++
@@ -1,9 +0,0 @@
-fin = open('words.txt')
-line = repr(fin.readline())
-
-def word_dict_txt():
-    word_dict = dict()
-    for line in fin:
-        word = line.strip()
-        word_dict[word] = word
-    return word_dict
